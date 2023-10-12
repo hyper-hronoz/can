bd2682148a4173c6bbde955e7dab7d96afaf6f60
hyper-hronoz <vlad_moryakov_prog@mail.ru>
2023-10-12 09:11:29 +0300

    refactoring stage all works

---
+++ Core/Inc/CAN_INRQ.h
@@ -0,0 +1,46 @@
+#ifndef __CAN_INRA__
+#define __CAN_INRA__
+
+#include "stm32f1xx.h"
+
+typedef struct {
+  uint8_t filter_bank1;
+  uint8_t filter_bank2;
+
+  uint8_t filter_position;
+
+  uint8_t list_mode_enable; // or else it will be mask mode
+
+  uint8_t scale_32_enable; // or else two 16bit banks
+
+} CAN_filter_INRQ;
+
+typedef struct {
+  uint8_t time_segment_1;
+  uint8_t time_segment_2;
+  uint32_t SJW;
+  uint32_t loop_back;
+  uint8_t prescaler;
+  uint8_t silent_mode;
+} CAN_bit_timing_INRQ;
+
+typedef struct {
+  uint16_t tx_ID;
+  uint8_t data_length;
+} CAN_Tx_INRQ;
+
+typedef struct {
+  uint16_t rx_ID;
+  uint8_t filter_id;
+  uint8_t data_length;
+} CAN_Rx_INRQ;
+
+typedef struct {
+  CAN_Tx_INRQ can_tx;
+  CAN_Rx_INRQ can_rx;
+  CAN_bit_timing_INRQ can_bit_timing;
+  CAN_filter_INRQ can_filter;
+} CAN_INRQ;
+
+
+#endif
---
+++ Core/Inc/Delay.h
@@ -0,0 +1,6 @@
+#ifndef __DELAY__
+#define __DELAY__
+
+
+
+#endif // !__DELAY__
